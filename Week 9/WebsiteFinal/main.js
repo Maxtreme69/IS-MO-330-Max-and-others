2a88c9a72726bf57c99cc1a1532f4dc202bbddf9
Maxtreme69 <62106005+Maxtreme69@users.noreply.github.com>
2020-09-20 21:03:23 +1000

    Add files via upload

---
+++ Week 9/WebsiteFinal/main.js
@@ -0,0 +1,9 @@
+
+/* Caraousel Slider toggle */
+
+$(document).ready(function(){
+  $('#carousel-one').carousel();
+  $('#carousel-two').carousel();
+  $('#carousel-three').carousel();
+  $('#carousel-four').carousel();
+})
